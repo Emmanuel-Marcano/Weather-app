db9d8aa449af15c15a052d7252e5a74ad071b7be
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-02-16 16:30:02 +0100

    added some simple error handling, must add a more strict one later

--- app.js
+++ app.js
@@ -15,40 +15,49 @@ const key = '854ca17890cc977ec26ea41c65550f95'
 
 form.addEventListener('submit', async function(e){
     e.preventDefault()
+    
+    appContainer.classList.remove("shake")
 
 
-    async function fetchWeather(){
-           let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&units=metric`)
-           let data = await response.json()
-           return data
+    if(input.value){
+        appContainer.style.borderColor = "white"
+        async function fetchWeather(){
+            let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${key}&units=metric`)
+            let data = await response.json()
+            return data
+     }
+ 
+     const weather = await fetchWeather()
+ 
+     console.log(weather)
+
+     switch(weather.weather[0].main) {
+        case "Clear":
+            weatherImage.src = "images/sun.png"
+          break;
+        case "Clouds":
+            weatherImage.src = "images/cloudy.png"
+          
+          break;
+
+        case "Haze":
+            weatherImage.src = "images/cloudy.png"
+          break;
+
+
+        default:
+          // code block
+      }
+
+        tempNumber.innerHTML = `${weather.main.temp}<sup> &#8451;</sup>`
+        tempDescription.innerText = weather.weather[0].description
+        humidity.innerText = `${weather.main.humidity}%`
+        windSpeed.innerText = `${weather.wind.speed}km/h`
+        appContainer.classList.add("expand")
+
+    } else {
+        input.placeholder = "Please enter a location"
+        appContainer.style.borderColor = "red"
+        appContainer.classList.add("shake")
     }
-
-    const weather = await fetchWeather()
-
-
-    console.log(weather)
-
-    if(weather.weather[0].main === "Clouds" || "Haze"){
-        weatherImage.src = "images/cloudy.png"
-    }
-
-    if(weather.weather[0].main === "Clear"){
-        weatherImage.src = "images/sun.png"
-    }
-
-
-
-     tempNumber.innerHTML = `${weather.main.temp}<sup> &#8451;</sup>`
-
-    tempDescription.innerText = weather.weather[0].description
-    humidity.innerText = `${weather.main.humidity}%`
-    windSpeed.innerText = `${weather.wind.speed}km/h`
-
-    appContainer.classList.add("expand")
-
 })
-
-
-
-
-

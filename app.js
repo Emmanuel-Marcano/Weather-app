55d512b7dd44a334e2971622080909e2af64e92b
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-02-17 16:17:55 +0100

    truncated temperature number

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
 const form = document.querySelector(".form")
 const appContainer = document.querySelector(".app-container")
 const weatherImage = document.querySelector(".weather-img")
@@ -10,21 +8,14 @@ const humidity = document.querySelector(".humidity-percentage")
 const windSpeed = document.querySelector(".wind-speed-number")
 const input = document.querySelector(".input")
 const placeName = document.querySelector(".place-name")
-
-
 const key = '854ca17890cc977ec26ea41c65550f95'
+
 
 form.addEventListener('submit', async function(e){
     e.preventDefault()
-
-    appContainer.classList.remove('shake')
-    
-
+    appContainer.classList.remove("shake")
     console.log("submitted")
     
-    
-
-
     if(input.value){
 
         appContainer.style.borderColor = "black"
@@ -73,24 +64,14 @@ form.addEventListener('submit', async function(e){
             weatherImage.src = "images/fog-2.png"
             break;
 
-        
-
         default:
           // code block
       }
 
-    //   let strArray = weather.weather[0].description.split("")
-    //   strArray[0] = strArray[0].toUpperCase()
-    //   for(let i = 0; i < strArray.length; i++){
-    //     if(strArray[i] == " "){
-    //        strArray[i+1] = strArray[i+1].toUpperCase()
-    //     }
-    //   }
-    //   let uppercasedDescription = strArray.join("")
      
-      
+
         placeName.innerText = weather.name
-        tempNumber.innerHTML = `${weather.main.temp}<sup> &#8451;</sup>`
+        tempNumber.innerHTML = `${Math.trunc(weather.main.temp)}<sup>&#8451;</sup>`
         tempDescription.innerText = weather.weather[0].description
         tempDescription.classList.add("capitalize")
         humidity.innerText = `${weather.main.humidity}%`
